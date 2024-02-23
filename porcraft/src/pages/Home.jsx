389f09c34df8b4448d830a784e0a766fc2546284
ichiro16go <ichi1.9adgjm@gmail.com>
2024-02-24 01:24:32 +0900

    change home and add cybercar

--- porcraft/src/pages/Home.jsx
+++ porcraft/src/pages/Home.jsx
@@ -1,10 +1,34 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState,useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const images=[
+    "./public/images/land1.png",
+     "./public/images/colorful.jpg",
+     "./public/images/future.jpg"
+    ];
+    const calculateBGPosition = (index)=>{
+      return `${index+100}% 50%`;
+    }
+
+  useEffect(()=>{
+    const interval = setInterval(() => {
+      setCurrentImage((prevImage)=>(prevImage+1) % images.length);
+    },2000);
+
+    return() => clearInterval(interval);
+  },[]);
+
   return (
-    <section className="w-full h-screen relative">
+    <section className="w-full h-screen relative"
+    style={{
+      backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)),url(${images[currentImage]})`,
+      backgroundSize:"cover",
+      backgroundPosition:calculateBGPosition(currentImage),
+      transition: "background-position 1s ease-in-out",
+    }}>
       <div
         className="absolute top-56 left-0 right-0 flex 
       items-center justify-center"
@@ -13,9 +37,11 @@ const Home = () => {
           <p className="start-text">
             Create your own portfolio with 3D objects!{" "}
           </p>
-          <Link to="/editor" className="btn">
+          <div className="flex">
+          <Link to="/editor" className="btn pading-all-20px">
             Get started!
           </Link>
+          </div>
         </div>
       </div>
     </section>
