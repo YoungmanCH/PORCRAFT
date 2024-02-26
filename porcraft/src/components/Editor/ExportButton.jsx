5af55d89c962ef4f4e7351d227f12042a066bb96
YoungmanCH <tsubasa22business@gmail.com>
2024-02-26 10:16:22 +0900

    Temporary save

--- porcraft/src/components/Editor/ExportButton.jsx
+++ porcraft/src/components/Editor/ExportButton.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+
 const ExportButton = ({ objects, serializeObjects }) => {
   const handleExport = async () => {
     const serializedData = serializeObjects(objects);
@@ -9,15 +10,10 @@ const ExportButton = ({ objects, serializeObjects }) => {
     const exportApiEndpoint =
       "https://y9x82tppo0.execute-api.ap-northeast-1.amazonaws.com/prod/export";
 
-    // データベースからデータを取得するためのAPIエンドポイント
-    // const getEndpoint =
-    //   "https://y9x82tppo0.execute-api.ap-northeast-1.amazonaws.com/prod/get";
-
     try {
       const response = await fetch(exportApiEndpoint, {
         method: "POST",
-        body: jsonData, // 直接serializedDataを使用
-        // body: serializedData, // 直接serializedDataを使用
+        body: jsonData,
         headers: {
           "Content-Type": "application/json",
         },
@@ -32,30 +28,6 @@ const ExportButton = ({ objects, serializeObjects }) => {
       const responseData = await response.json();
       console.log("responseData:", responseData);
 
-      // // 保存されたデータを取得
-      // const fetchResponse = await fetch(getEndpoint, {
-      //   method: "GET",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      // });
-
-      // if (!fetchResponse.ok) {
-      //   throw new Error("Network response was not ok");
-      // }
-
-      // const fetchedData = await fetchResponse.json();
-      // console.log("Fetched data:", fetchedData);
-
-      // const bodyData = fetchedData.body;
-      // console.log("Parsed body data:", bodyData);
-
-
-      // bodyDataを使って何か処理を行う
-      // bodyData.map((item, index) => {
-      //   console.log(`Item ${index}:`, item);
-      //   // ここで各アイテムの詳細な処理を行う
-      // });
     } catch (error) {
       console.error("Error:", error);
     }
