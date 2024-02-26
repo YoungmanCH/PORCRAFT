0c8651ece7e8c8482eefb2d5e1adce76a485add7
YoungmanCH <tsubasa22business@gmail.com>
2024-02-26 21:25:23 +0900

    Update package.json

--- porcraft/vite.config.js
+++ porcraft/vite.config.js
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  build: {minify: false}
+  build: {minify: false},
+  server: {
+    host: true,
+  }
 })
