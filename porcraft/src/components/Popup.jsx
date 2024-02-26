c1c8a92870a863ee3b8447b67c9c54edd6fa22e2
ichiro16go <ichi1.9adgjm@gmail.com>
2024-02-26 15:41:59 +0900

    adapt islands size

--- porcraft/src/components/Popup.jsx
+++ porcraft/src/components/Popup.jsx
@@ -1,17 +1,37 @@
 import React from 'react'
+import { useState } from 'react';
 
-const Popup = () => {
-  return (
-    <section className='h-5/6 w-5/6 relaive'>
-        <div style={{
-            width:"100%",
-            height:"100%",
-            backgroundColor:"white",
-        }}>
-
-        </div>
+const Popup = ({setIsPopupVisible}) => {
+  
+    return (
+    
+    <section style={{
+      width: '90vw',
+      height: '90vh',
+      backgroundColor: 'white',
+      position: 'fixed',
+      top: '5vh',
+      left: '5vw',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius:'20px',
+      zIndex: 1000, // ポップアップが他の要素より前面に来るように
+    }}>
+        <button onClick={setIsPopupVisible(false)} style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        borderRadius: '0 0 5px 0', // ボタンの右下の角を丸くする
+      }}>
+        X
+      </button>
+      
+        <h3>hhh</h3>
+           {/* ここにポップアップの内容を追加 */}
     </section>
   )
-}
+    }
+    
 
-export default Button
+export default Popup
--- porcraft/src/pages/Editor.jsx
+++ porcraft/src/pages/Editor.jsx
@@ -1,8 +1,11 @@
 import { useLocation } from "react-router-dom";
+import React,{useState} from "react";
+
 import EditorCanvas from "../components/Editor/EditorCanvas";
 import ExportButton from "../components/Editor/ExportButton";
 import ObjectSelector from "../components/Editor/ObjectSelector";
 import ObjectSetting from "../components/Editor/ObjectSetting";
+import Popup from "../components/Popup";
 
 import useAddedObjects from "../features/UseAddedObjects";
 
@@ -16,7 +19,7 @@ const Editor = () => {
     removeObject,
     serializeObjects,
   ] = useAddedObjects();
-
+  const [isPopupVisible,setIsPopupVisible]=useState(false);
   const location = useLocation();
   const { fieldName } = location.state || {}; 
 
@@ -28,9 +31,21 @@ const Editor = () => {
       {/* Exportボタン */}
       <ExportButton objects={objects} serializeObjects={serializeObjects} />
 
+      <div className="btn">
+        {/* ポップアップを開くボタン */}
+        <button onClick={() => setIsPopupVisible(true)}>ポップアップを開く</button>
+
+      {/* ポップアップの表示 */}
+        {isPopupVisible && <Popup setIsVisible={setIsPopupVisible}/>}
+      </div>
+
       {/* 3Dエディタエリア */}
       <div className="flex-grow h-full">
         <EditorCanvas fieldName={fieldName} objects={objects} setScale={setScale} />
+
+        <Popup/>
+    
+
       </div>
 
       {/* オブジェクト設定エリア */}
