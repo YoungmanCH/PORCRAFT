4a0d7dd3fafaee2af520e953d8f62f8e1e570cf1
YoungmanCH <tsubasa22business@gmail.com>
2024-06-01 10:55:47 +0900

    Added: Authentication UI viewer.

--- porcraft/src/main.jsx
+++ porcraft/src/main.jsx
@@ -2,5 +2,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
--- porcraft/vite.config.js
+++ porcraft/vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb'],
-  build: {minify: false},
+  assetsInclude: ["**/*.glb"],
+  build: { minify: false },
   server: {
     host: true,
-  }
-})
+  },
+  optimizeDeps: {
+    include: ["aws-amplify"],
+  },
+});
